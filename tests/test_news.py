c02292035191624abecaa81beaffbd313025da1e
ombima452 <christineombima452@gmail>
2019-09-10 21:51:29 +0300

    test articles

--- tests/test_news.py
+++ tests/test_news.py
@@ -24,25 +24,25 @@ class SourcesTest(unittest.TestCase):
         self.assertEquals(self.new_source.country,'U.S.A')
         self.assertEquals(self.new_source.language,'en')
 
-# class ArticlesTest(unittest.TestCase):
-#     '''
-#     Test Class to test the behaviour of the Movie class
-#     '''
+class ArticlesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
 
-#     def setUp(self):
-#         '''
-#         Set up method that will run before every Test
-#         '''
-#         self.new_article = Articles('CNN','ombima','The tech scene in Africa','A look at various tech hubs in Africa ','techie.com','techie.com/7643t94.jpg','201-09-11T07:57:16Z')
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Articles('CNN','ombima','The tech scene in Africa','A look at various tech hubs in Africa ','techie.com','techie.com/7643t94.jpg','201-09-11T07:57:16Z')
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_article,Articles))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Articles))
 
-#     def test_to_check_instance_variables(self):
-#         self.assertEquals(self.new_article.id,'CNN')
-#         self.assertEquals(self.new_article.author,'ombima')
-#         self.assertEquals(self.new_article.title,'The tech scene in Africa')
-#         self.assertEquals(self.new_article.description,'A look at various tech hubs in Africa')
-#         self.assertEquals(self.new_article.url,'techie.com')
-#         self.assertEquals(self.new_article.image,'techie.com/7643t94.jpg')
-#         self.assertEquals(self.new_article.date,'2019-09-11T07:57:16Z')
+    def test_to_check_instance_variables(self):
+        self.assertEquals(self.new_article.id,'CNN')
+        self.assertEquals(self.new_article.author,'ombima')
+        self.assertEquals(self.new_article.title,'The tech scene in Africa')
+        self.assertEquals(self.new_article.description,'A look at various tech hubs in Africa')
+        self.assertEquals(self.new_article.url,'techie.com')
+        self.assertEquals(self.new_article.image,'techie.com/7643t94.jpg')
+        self.assertEquals(self.new_article.date,'2019-09-11T07:57:16Z')
